0614de767f3b65c89a78e1792405d6d49196e784
benyamin <60822041+wyznel@users.noreply.github.com>
2026-07-15 22:16:16 +0100

    change: Change pullModel() to return a stream instead of Bool. Allows viewing of download progress.

--- Sources/Ollama/Client.swift
+++ Sources/Ollama/Client.swift
@@ -925,6 +925,13 @@ extension Client {
 
 // MARK: - Pull Model
 
+public struct PullProgress: Decodable, Sendable {
+    public let status: String
+    public let digest: String?
+    public let total: Int?
+    public let completed: Int?
+}
+
 extension Client {
     /// Downloads a model from the Ollama library.
     ///
@@ -938,15 +945,13 @@ extension Client {
     public func pullModel(
         _ id: Model.ID,
         insecure: Bool = false
-    )
-        async throws -> Bool
-    {
+    ) -> AsyncThrowingStream<PullProgress, Swift.Error> {
         let params: [String: Value] = [
             "name": .string(id.rawValue),
             "insecure": .bool(insecure),
-            "stream": false,
+            "stream": true,
         ]
-        return try await fetch(.post, "/api/pull", params: params)
+        return fetchStream(.post, "/api/pull", params: params)
     }
 }
 
